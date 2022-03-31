a9a6d56378895e1b3e4a22650cd5af5cae6f444d
Victor Kasap <kasap.victor@gmail.com>
2022-03-31 07:00:23 +0300

    feat: added responsive + watched state

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -1,3 +1,6 @@
+import onChange from 'on-change';
+import _ from 'lodash';
+import axios from 'axios';
 /*
 NAME
 https://api.openweathermap.org/data/2.5/find?q=moscow&appid=3eeda7da43f6704e77308a8aceb09b0f&units=metric&lang=ru
@@ -9,6 +12,30 @@ GROUP
 http://api.openweathermap.org/data/2.5/group?id=524894,498817&appid=3eeda7da43f6704e77308a8aceb09b0f&units=metric
 */
 
+const render = (state, elements) => (path, value, prevValue) => {
+  console.log('PATH', path);
+  console.log('VALUE', value);
+  console.log('PREV', prevValue);
+};
+
+const formHandler = (e, form, watchedState) => {
+  e.preventDefault();
+
+  const formData = new FormData(form);
+  const inputSearchValue = formData.get('search').trim();
+
+  console.log(inputSearchValue);
+};
+
 export default () => {
-  console.log('app js');
+  const elements = {
+    form: document.forms['form-search'],
+  };
+
+  const state = {};
+
+  const watchedState = onChange(state, render(state, elements));
+
+  const { form } = elements;
+  form.addEventListener('submit', e => formHandler(e, form, watchedState));
 };
